275b02a4a77058935dcd48a01717ff148a0d2a35
Al Zaplin <zaplin.dev@gmail.com>
2024-06-29 01:30:19 +0300

    upd(doc) Add array methods cheatsheet

--- script.js
+++ script.js
@@ -22,3 +22,30 @@ const isDateValid = (...val) => !Number.isNaN(new Date(...val).valueOf());
 
 // Вычисление дней между двумя датами
 const dayDiff = (date1, date2) => Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / 86400000);
+
+// __________________ \\
+// * === Arrays === * \\
+
+[1, 2, 3].push(4); // [1, 2, 3, 4]
+[1, 2, 3].pop(); // [1, 2]
+[1, 2, 3].shift(); // [2, 3]
+[1, 2, 3].unshift(0); // [0, 1, 2, 3]
+[1, 2, 3].concat([4, 5]); // [0, 1, 2, 3, 4, 5]
+[1, 2, 3].join('-'); // 1-2-3
+[1, 2, 3].slice(1) // [2, 3]
+[1, 2, 3].splice(2, 1, 5) // [1, 2, 5]
+[1, 2, 3].indexOf(2) // 1
+[1, 2, 3, 1].lastIndexOf(1) // 3
+[1, 2, 3].at(0) // 1
+[1, 2, 3].includes(3) // true
+[1, 2, 3].find(n => n % 2 === 0) // 2
+[1, 2, 3].findIndex(n => n % 2 === 0) // 1
+[1, 2, 3].map(n => n * 2) // [2, 4, 6]
+[1, 2, 3].filter(n => n % 2 !== 0) // [1, 3]
+[1, 2, 3].reduce((acc, cur) => acc + cur) // 6
+[1, 2, 3].fill(0, 1) // [1, 0, 0]
+[1, 2, 3].every(n => n > 0) // true
+[1, 2, 3].some(n => n > 2) // true
+[1, 2, 3].reverse() // [3, 2, 1]
+[2, 1, 3].sort((a, b) => a - b) // [1, 2, 3]
+
